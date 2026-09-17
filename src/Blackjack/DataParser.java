af7f7d019ba1e5fb6f56870f661c7ecd990b0dff
itsung26 <iat2016@icloud.com>
2026-09-17 15:56:15 -0400

    Update DataParser.java

--- src/Blackjack/DataParser.java
+++ src/Blackjack/DataParser.java
@@ -1,4 +1,38 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class DataParser {
+    private Properties propertiesInstance;
+
+    public DataParser() {
+        propertiesInstance = new Properties();
+    }
+
+    public void Save(String fileName) throws IOException {
+        if (fileName == null || fileName == "") {
+            return;
+        }
+
+        propertiesInstance.store(new FileOutputStream(fileName), null);
+    }
+
+    public void Load(String fileName) throws IOException {
+        if (fileName == null || fileName == "") {
+            return;
+        }
+
+        propertiesInstance.load(new FileInputStream(fileName));
+    }
+
+    public void setProperty(String key, String value) {
+        propertiesInstance.setProperty(key, value);
+    }
+
+    public String getProperty(String key, String fallback) {
+        return propertiesInstance.getProperty(key, fallback);
+    }
+
 
 }
