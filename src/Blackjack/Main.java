def14b7a49c160f1f26cb771b89560cbe13fe120
christopherbei <christopherbei@vt.edu>
2026-09-21 20:05:19 -0400

    start of game/main files
    also tiny typo in deck class fixed

--- src/Blackjack/Main.java
+++ src/Blackjack/Main.java
@@ -2,8 +2,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// run stuff
-
+		BlackjackGame game = new BlackjackGame();
+		
+		
+		private void onFirstLoop() {
+		    
+		}
 	}
 
 }
