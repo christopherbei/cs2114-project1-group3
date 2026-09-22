4975b0bf78b51ad13d9a003b33fd92d3053f9ddb
Adamb06VT <adamb06@vt.edu>
2026-09-21 20:55:30 -0400

    Updated Dealer and Player class to include methos and constructors

--- src/Blackjack/Dealer.java
+++ src/Blackjack/Dealer.java
@@ -1,4 +1,30 @@
+imporat java.util.*;
 
-public class Dealer {
+public class Dealer
+{
+    private Hand() dealerHand;
 
+
+    public Dealer()
+    {
+        dealerHand = new Hand();
+    }
+
+
+    public void addCard(Card card)
+    {
+        dealerHand.addCard(card);
+        if (dealerHand.size() == 2) {
+            System.out.println("*");
+        }
+        else {
+            System.out.println(dealerHand.toString());
+        }
+    }
+
+
+    public boolean busted()
+    {
+        return dealerHand.totalValue() > 21;
+    }
 }
--- src/Blackjack/Player.java
+++ src/Blackjack/Player.java
@@ -1,4 +1,44 @@
+import java.util.*;
 
-public class Player {
+/**
+ * // -------------------------------------------------------------------------
+ * /** Write a one-sentence summary of your class here. Follow it with
+ * additional details about its purpose, what abstraction it represents, and how
+ * to use it.
+ * 
+ * @author adambergmaier
+ * @version Sep 21, 2026
+ */
+public class Player
+{
+
+    private int balance;
+    private int bid;
+    private Hand playerHand;
+
+    public Player(int balance)
+    {
+        playerHand = new Hand();
+        this.balance = balance;
+    }
+
+
+    public int getBalance()
+    {
+        return balance;
+    }
+
+
+    public boolean busted()
+    {
+        return playerHand.totalValue() > 21;
+    }
+
+
+    public void addCard(Card card)
+    {
+        playerHand.addCard(card);
+        System.out.println(playerHand.toString());
+    }
 
 }
