0c2b05d91ba2cdf787f8d3f7f1e25763091b0edf
christopherbei <christopherbei@vt.edu>
2026-09-21 20:22:18 -0400

    basic game class
    early version of handleinput method

--- src/Blackjack/BlackjackGame.java
+++ src/Blackjack/BlackjackGame.java
@@ -1,28 +1,69 @@
 
-public class BlackjackGame {
+import java.util.Scanner;
+
+public class BlackjackGame
+{
+    private Scanner input;
     private int currentBid;
-    
-    Deck deck;
-    Player player;
-    Dealer dealer;
-    
-    public BlackjackGame() {
+
+    private Deck deck;
+    private Player player;
+    private Dealer dealer;
+
+    public BlackjackGame()
+    {
+        input = new Scanner(System.in);
         currentBid = 1000;
-        
+
         deck = new Deck();
         player = new Player();
         dealer = new Dealer();
     }
-    
-    public void runCommand(String action) {
-        
+
+
+    public void runCommand(String action)
+    {
+        // unused
     }
-    
-    public void pauseConsole(double duration) {
-        
+
+
+    public void pauseConsole(double duration)
+    {
+        // unused
     }
-    
-    public String handleInput() {
+
+
+    public String handleInput()
+    {
+        boolean complete = false;
+        String validActions = "";
         
+        System.out.println("please enter your action: ");
+        while (!complete)
+        {
+            String action = input.nextLine();
+            action = action.toLowerCase();
+            
+            switch (action)
+            {
+                case "hit":
+                    player.addCard(deck.remove());
+                    complete = true;
+                    break;
+                case "stand":
+                    // implement
+                    complete = true;
+                    break;
+                case "stop":
+                    // implement
+                    complete = true;
+                    break;
+                default:
+                    System.out.println("invalid input, try again: ");
+            }
+        }
     }
+
+
+    handleInput();
 }
