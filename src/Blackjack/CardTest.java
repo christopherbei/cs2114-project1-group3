6e28787915f6f975ad76b6c227b3278b4d00423f
christopherbei <christopherbei@vt.edu>
2026-09-23 13:22:38 -0400

    test class start
    just the setup function for rmy three classes

---
+++ src/Blackjack/CardTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+// -------------------------------------------------------------------------
+/**
+ * Test cases for the Card class
+ * 
+ * @author Chris
+ * @version 23 Sep 2026
+ */
+public class CardTest
+{
+    private Card card;
+
+    @Before
+    public void setUp()
+    {
+
+    }
+}
